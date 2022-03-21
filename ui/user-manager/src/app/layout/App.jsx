5edb6c9ba27f6743c8f55838b7de2390d9630060
Jobin Basani <jobin.basani@arcticwolf.com>
2022-03-21 14:27:42 -0300

    Add NavBar and styles

--- ui/user-manager/src/app/layout/App.jsx
+++ ui/user-manager/src/app/layout/App.jsx
@@ -1,9 +1,16 @@
+import EventDashboard
+  from "../../features/events/eventDashboard/EventDashboard";
+import NavBar from '../../features/nav/NavBar';
+import { Container } from 'semantic-ui-react';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Re-Events</h1>
-    </div>
+    <>
+      <NavBar/>
+      <Container className='main'>
+        <EventDashboard />
+      </Container>
+    </>
   );
 }
 
