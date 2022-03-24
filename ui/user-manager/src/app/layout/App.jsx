7c74f0f88a603707489dccf7cc9eb35eac5fad02
Jobin Basani <jobin.basani@arcticwolf.com>
2022-03-24 16:15:05 -0300

    Hook event forms

--- ui/user-manager/src/app/layout/App.jsx
+++ ui/user-manager/src/app/layout/App.jsx
@@ -2,13 +2,16 @@ import EventDashboard
   from "../../features/events/eventDashboard/EventDashboard";
 import NavBar from '../../features/nav/NavBar';
 import { Container } from 'semantic-ui-react';
+import React,{ useState } from 'react';
 
 function App() {
+  const [formOpen, setFormOpen] = useState(false);
+
   return (
     <>
-      <NavBar/>
+      <NavBar setFormOpen={setFormOpen} />
       <Container className='main'>
-        <EventDashboard />
+        <EventDashboard formOpen={formOpen} setFormOpen={setFormOpen} />
       </Container>
     </>
   );
