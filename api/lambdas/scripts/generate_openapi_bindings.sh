705d7f6cf22d3cdf85c047664f4b24295cfa9498
Jobin Basani <jobin.basani@arcticwolf.com>
2022-04-09 17:51:08 -0300

    Add an api spec

---
+++ api/lambdas/scripts/generate_openapi_bindings.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+rootdir=$(cd $(dirname ${BASH_SOURCE[0]}); cd ../; pwd)
+
+PACKAGE_NAME=openapi
+WORKDIR=${rootdir}/user_manager/${PACKAGE_NAME}
+GENERATOR_IMAGE=openapitools/openapi-generator-cli:v5.4.0
+
+docker run --rm -v "$rootdir":/src_files "$GENERATOR_IMAGE" generate \
+  -i /src_files/api_spec.yml \
+  -g go-server \
+  -o /src_files/user_manager/${PACKAGE_NAME} \
+  --package-name=${PACKAGE_NAME}
+
+mv $WORKDIR/go/* $WORKDIR
+rm -Rf $WORKDIR/go
+rm -Rf $WORKDIR/api
