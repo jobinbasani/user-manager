4340296efa2f0872b39131670d623d7e2e155ae8
Jobin Basani <jobin.basani@arcticwolf.com>
2022-04-09 15:35:34 -0300

    Fix paths

--- ui/user-manager/src/routes.tsx
+++ ui/user-manager/src/routes.tsx
@@ -1,9 +1,8 @@
-
 /*
 JJ: Routes definition
 */
 
-import { useRoutes } from "react-router-dom";
+import {useRoutes} from "react-router-dom";
 import NotFound from "./components/unspported/NotFound";
 import Catechism from "./pages/public/Catechism";
 import Committee from "./pages/public/Committee";
@@ -14,20 +13,19 @@ import Login from "./pages/public/Login";
 import Services from "./pages/public/Services";
 
 const Router = () => {
-    let element = useRoutes(
+  return useRoutes(
         [
-            {path: "*", element: <NotFound />},
-            {path: "/", element: <Home />},
-            {path: "home", element: <Home />},
-            {path: "committee", element: <Committee />},
-            {path: "services", element: <Services/>},
-            {path: "catechism", element: <Catechism/>},
-            {path: "gallery", element: <Gallery/>},
-            {path: "register", element: <JoinParish/>},
-            {path: "login", element: <Login/>}
+          {path: "*", element: <NotFound/>},
+          {path: "/", element: <Home/>},
+          {path: "home", element: <Home/>},
+          {path: "committee", element: <Committee/>},
+          {path: "services", element: <Services/>},
+          {path: "catechism", element: <Catechism/>},
+          {path: "gallery", element: <Gallery/>},
+          {path: "register", element: <JoinParish/>},
+          {path: "login", element: <Login/>}
         ]
     );
-    return element;
 };
 
 export default Router;
