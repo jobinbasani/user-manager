7b2e8fb9008df2c52c9423233227a6794fdbaf91
Jobin Basani <jobin.basani@arcticwolf.com>
2022-04-12 10:26:54 -0300

    Add API GW origin to Cloudfront

--- infra/lib/user-manager-stack.js
+++ infra/lib/user-manager-stack.js
@@ -2,7 +2,10 @@ const {
   Stack,
   Duration,
 } = require('aws-cdk-lib');
-const { CloudFrontWebDistribution, OriginAccessIdentity } = require('aws-cdk-lib/aws-cloudfront');
+const {
+  CloudFrontWebDistribution, OriginAccessIdentity,
+  OriginProtocolPolicy, CloudFrontAllowedMethods,
+} = require('aws-cdk-lib/aws-cloudfront');
 const {
   PolicyStatement,
   Role,
@@ -37,51 +40,6 @@ class UserManagerStack extends Stack {
       value: s3Bucket.bucketName,
       description: 'S3 Bucket that holds the web application',
     });
-
-    const cloudFrontOAI = new OriginAccessIdentity(this, 'OAI', {
-      comment: 'OAI for User Manager website.',
-    });
-
-    const distribution = new CloudFrontWebDistribution(this, 'UserManagerCDN', {
-      originConfigs: [
-        {
-          s3OriginSource: {
-            s3BucketSource: s3Bucket,
-            originAccessIdentity: cloudFrontOAI,
-          },
-          behaviors: [{ isDefaultBehavior: true }],
-        },
-      ],
-      defaultRootObject: 'main/index.html',
-    });
-
-    const cdnUrl = new cdk.CfnOutput(this, 'CdnUrl', {
-      value: `https://${distribution.distributionDomainName}/`,
-      description: 'Cloudfront URL',
-    });
-
-    const cdnId = new cdk.CfnOutput(this, 'CdnId', {
-      value: distribution.distributionId,
-      description: 'Cloudfront Distribution ID',
-    });
-
-    const cloudfrontS3Access = new PolicyStatement({
-      actions: [
-        's3:GetBucket*',
-        's3:GetObject*',
-        's3:List*',
-      ],
-      resources: [
-        s3Bucket.bucketArn,
-        `${s3Bucket.bucketArn}/*`,
-      ],
-    });
-
-    cloudfrontS3Access.addCanonicalUserPrincipal(
-      cloudFrontOAI.cloudFrontOriginAccessIdentityS3CanonicalUserId,
-    );
-
-    s3Bucket.addToResourcePolicy(cloudfrontS3Access);
 
     const userTable = new Table(this, id, {
       tableName: 'UserDetails',
@@ -141,6 +99,65 @@ class UserManagerStack extends Stack {
       description: 'userManager ApiGateway URL',
     });
 
+    const cloudFrontOAI = new OriginAccessIdentity(this, 'OAI', {
+      comment: 'OAI for User Manager website.',
+    });
+
+    const apiEndPointDomainName = cdk.Fn.parseDomainName(userManagerApiGateway.url);
+
+    const distribution = new CloudFrontWebDistribution(this, 'UserManagerCDN', {
+      originConfigs: [
+        {
+          s3OriginSource: {
+            s3BucketSource: s3Bucket,
+            originAccessIdentity: cloudFrontOAI,
+          },
+          behaviors: [{ isDefaultBehavior: true }],
+        },
+        {
+          customOriginSource: {
+            domainName: apiEndPointDomainName,
+            originProtocolPolicy: OriginProtocolPolicy.MATCH_VIEWER,
+          },
+          behaviors: [
+            {
+              pathPattern: '/api/*',
+              allowedMethods: CloudFrontAllowedMethods.ALL,
+            },
+          ],
+        },
+      ],
+      defaultRootObject: 'main/index.html',
+    });
+
+    const cdnUrl = new cdk.CfnOutput(this, 'CdnUrl', {
+      value: `https://${distribution.distributionDomainName}/`,
+      description: 'Cloudfront URL',
+    });
+
+    const cdnId = new cdk.CfnOutput(this, 'CdnId', {
+      value: distribution.distributionId,
+      description: 'Cloudfront Distribution ID',
+    });
+
+    const cloudfrontS3Access = new PolicyStatement({
+      actions: [
+        's3:GetBucket*',
+        's3:GetObject*',
+        's3:List*',
+      ],
+      resources: [
+        s3Bucket.bucketArn,
+        `${s3Bucket.bucketArn}/*`,
+      ],
+    });
+
+    cloudfrontS3Access.addCanonicalUserPrincipal(
+      cloudFrontOAI.cloudFrontOriginAccessIdentityS3CanonicalUserId,
+    );
+
+    s3Bucket.addToResourcePolicy(cloudfrontS3Access);
+
     const userPool = new UserPool(this, 'userpool', {
       userPoolName: 'user-manager-user-pool',
       selfSignUpEnabled: true,
@@ -191,15 +208,15 @@ class UserManagerStack extends Stack {
     };
 
     const clientReadAttributes = new ClientAttributes()
-      .withStandardAttributes(standardCognitoAttributes)
-      .withCustomAttributes(...['isAdmin']);
+    .withStandardAttributes(standardCognitoAttributes)
+    .withCustomAttributes(...['isAdmin']);
 
     const clientWriteAttributes = new ClientAttributes()
-      .withStandardAttributes({
-        ...standardCognitoAttributes,
-        emailVerified: false,
-        phoneNumberVerified: false,
-      });
+    .withStandardAttributes({
+      ...standardCognitoAttributes,
+      emailVerified: false,
+      phoneNumberVerified: false,
+    });
 
     const userPoolClient = new UserPoolClient(this, 'user-manager-userpool-client', {
       userPool,
