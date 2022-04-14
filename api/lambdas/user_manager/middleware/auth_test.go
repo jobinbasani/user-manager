de2b90a7207d6453296244fd154ba47a17fef09f
Jobin Basani <jobinbasani@gmail.com>
2022-04-14 09:11:06 -0300

    Fix tests

--- api/lambdas/user_manager/middleware/auth_test.go
+++ api/lambdas/user_manager/middleware/auth_test.go
@@ -6,11 +6,14 @@ import (
 	"crypto/rsa"
 	"encoding/json"
 	"fmt"
+	"github.com/lestrrat-go/jwx/v2/jwa"
+	"github.com/lestrrat-go/jwx/v2/jwt"
 	"lambdas/user_manager/config"
 	"net/http"
 	"net/http/httptest"
 	"net/url"
 	"testing"
+	"time"
 
 	"github.com/lestrrat-go/jwx/v2/jwk"
 	"github.com/stretchr/testify/require"
@@ -24,8 +27,11 @@ func Test(t *testing.T) {
 	key, err := jwk.FromRaw(rsaKey)
 	err = key.Set(jwk.KeyIDKey, "testkey")
 	require.NoError(t, err)
-
-	buf, err := json.MarshalIndent(key, "", "  ")
+	err = key.Set(jwk.AlgorithmKey, jwa.RS256)
+	require.NoError(t, err)
+	publicKey, err := key.PublicKey()
+	require.NoError(t, err)
+	buf, err := json.MarshalIndent(publicKey, "", "  ")
 	require.NoError(t, err)
 
 	srv := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
@@ -88,4 +94,25 @@ func Test(t *testing.T) {
 		t.Errorf("handler returned wrong status code: got %v want %v",
 			status, http.StatusOK)
 	}
+
+	testJwt, err := jwt.NewBuilder().
+		Subject("test_sub").
+		IssuedAt(time.Now()).
+		Expiration(time.Now().Add(time.Hour * 2)).
+		Build()
+	require.NoError(t, err)
+
+	signed, err := jwt.Sign(testJwt, jwt.WithKey(jwa.RS256, key))
+	require.NoError(t, err)
+
+	req, err = http.NewRequest(http.MethodGet, "/test/request", nil)
+	req.Header.Add("Authorization", fmt.Sprintf("Bearer %s", string(signed)))
+
+	DoAuth(context.Background(), cfg)(nil).ServeHTTP(rr, req)
+
+	if status := rr.Code; status != http.StatusUnauthorized {
+		t.Errorf("handler returned wrong status code: got %v want %v",
+			status, http.StatusOK)
+	}
+
 }
