d6d3a7621685ac5a80f0cd1eb6bbfe410a818592
Jobin Basani <jobin.basani@arcticwolf.com>
2022-04-15 21:57:58 -0300

    Update login url

--- ui/user-manager/src/constants/ApiConstants.ts
+++ ui/user-manager/src/constants/ApiConstants.ts
@@ -1,3 +1,3 @@
 
 export const API_URL = "";
-export const LOGIN_URL = "https://hfxchurch.auth.ca-central-1.amazoncognito.com/oauth2/authorize?client_id=76tmogtonieiq0opkdcemoho7v&redirect_uri=http%3A%2F%2Flocalhost%3A3000%2Fcallback&scope=openid&response_type=token";
+export const LOGIN_URL = "https://user-manager.auth.ca-central-1.amazoncognito.com/login?client_id=28rnopv48gu9m8pv9525tbelro&redirect_uri=http%3A%2F%2Flocalhost%3A3000%2Fcallback&scope=aws.cognito.signin.user.admin+openid&response_type=token";
