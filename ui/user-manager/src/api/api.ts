1d3c365a373b25c5417732de10f9032ba7229e95
Jubin Jose <jubinpolackal@gmail.com>
2022-05-03 23:02:57 -0300

    Fetch user details - modifications

--- ui/user-manager/src/api/api.ts
+++ ui/user-manager/src/api/api.ts
@@ -1,24 +1,24 @@
-import axios, { AxiosRequestConfig } from "axios"
+import axios, { AxiosRequestConfig, AxiosRequestHeaders } from "axios"
 import { API_URL } from '../constants/ApiConstants';
 import { authHeader } from './auth-header';
-import { AxiosRequestHeaders } from 'axios';
 
 
-export default class UserManagerApi {
-    static apiHeader: AxiosRequestHeaders = authHeader();
-    static requestConfig: AxiosRequestConfig = {headers: this.apiHeader};
-    static headers = {
-        headers: authHeader()
-    }
+const getApiHeader = (): AxiosRequestConfig => {
+    const apiHeader: AxiosRequestHeaders = authHeader();
+    const requestConfig: AxiosRequestConfig = {headers: apiHeader};
 
-    /** Get user details */
-    static getUserDetails = async () => {
-        console.dir(this.requestConfig);
-        axios.get(API_URL+"user/profile", this.requestConfig).then((response) => {
-            console.log(response);
-            return response;
-        }).catch(error => {
-            console.log(error);
-        }) ;
-    }
+    return requestConfig;
 }
+
+
+/** Get user details */
+ export const getUserDetails = async () => {
+    let header = getApiHeader();
+    console.dir(header);
+    axios.get(API_URL+"user/profile", header).then((response) => {
+        console.log(response);
+        return response;
+    }).catch(error => {
+        console.log(error);
+    }) ;
+ }
--- ui/user-manager/src/store/user/user-action.ts
+++ ui/user-manager/src/store/user/user-action.ts
@@ -1,9 +1,9 @@
-import UserManagerApi from '../../api/api';
+import { getUserDetails } from '../../api/api';
 
 /** Fetch user details action */
 export const getUserInfo = () => {
     return async (dispatch: any) => {
-        let userData = await UserManagerApi.getUserDetails();
+        let userData = await getUserDetails();
         console.dir(userData);
     }
 }
