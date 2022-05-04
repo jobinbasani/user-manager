9c0ee55ca842492e9eaafb957422ec015cd7d398
Jobin Basani <jobin.basani@arcticwolf.com>
2022-05-04 11:39:15 -0300

    Set default route

--- ui/user-manager/src/routes.tsx
+++ ui/user-manager/src/routes.tsx
@@ -1,10 +1,9 @@
-
 /*
 JJ: Routes definition
 */
 
-import { useSelector } from "react-redux";
-import { Navigate, useRoutes } from "react-router-dom";
+import {useSelector} from "react-redux";
+import {Navigate, useRoutes} from "react-router-dom";
 import Unauthorized from "./components/unspported/Unauthorized";
 import Dashboard from "./pages/private/Dashboard";
 import Catechism from "./pages/public/Catechism";
@@ -13,28 +12,27 @@ import Gallery from "./pages/public/Gallery";
 import Home from "./pages/public/Home";
 import Login from "./pages/public/Login";
 import Services from "./pages/public/Services";
-import { RootState } from './store/index';
+import {RootState} from './store/index';
 import Register from './pages/public/Register';
 
 const Router = () => {
     const isLoggedIn = useSelector((state: RootState) => state.auth.isAuthenticated);
 
-    let element = useRoutes(
+  return useRoutes(
         [
-            {path: "home", element: <Home />},
-            {path: "committee", element: <Committee />},
-            {path: "services", element: <Services/>},
-            {path: "catechism", element: <Catechism/>},
-            {path: "gallery", element: <Gallery/>},
-            {path: "login", element: <Login />},
-            {path: "register", element: <Register />},
-            {path: "callback", element: <Login />},
-            {path: "unauthorized", element: <Unauthorized />},
-            {path: "dashboard", element: isLoggedIn? <Dashboard /> : <Navigate to="/home" />},
-            {path: "*", element: <Navigate to="dashboard" replace />}
+          {path: "home", element: <Home/>},
+          {path: "committee", element: <Committee/>},
+          {path: "services", element: <Services/>},
+          {path: "catechism", element: <Catechism/>},
+          {path: "gallery", element: <Gallery/>},
+          {path: "login", element: <Login/>},
+          {path: "register", element: <Register/>},
+          {path: "callback", element: <Login/>},
+          {path: "unauthorized", element: <Unauthorized/>},
+          {path: "dashboard", element: isLoggedIn ? <Dashboard/> : <Navigate to="/home"/>},
+          {path: "*", element: <Navigate to="dashboard" replace/>}
         ]
     );
-    return element;
 };
 
 export default Router;
