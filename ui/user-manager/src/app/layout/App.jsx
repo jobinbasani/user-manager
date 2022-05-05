10f8c37ebea3c1d213d08812962740cc683e9875
Jobin Basani <jobin.basani@arcticwolf.com>
2022-05-05 12:20:15 -0300

    Add sandbox code

--- ui/user-manager/src/app/layout/App.jsx
+++ ui/user-manager/src/app/layout/App.jsx
@@ -6,6 +6,7 @@ import { Route } from 'react-router-dom';
 import HomePage from '../../features/home/HomePage';
 import EventDetailedPage from '../../features/events/eventsDetailed/EventDetailedPage';
 import EventForm from '../../features/events/eventForm/EventForm';
+import Sandbox from '../../features/sandbox/Sandbox';
 
 function App() {
 
@@ -17,6 +18,7 @@ function App() {
           <NavBar />
           <Container className='main'>
             <Route path='/events' exact component={EventDashboard} />
+            <Route path='/sandbox' exact component={Sandbox} />
             <Route path='/events/:id' component={EventDetailedPage} />
             <Route path={['/createEvent', '/manage/:id']} component={EventForm} />
           </Container>
--- ui/user-manager/src/index.js
+++ ui/user-manager/src/index.js
@@ -4,11 +4,17 @@ import 'semantic-ui-css/semantic.min.css'
 import './app/layout/styles.css';
 import App from './app/layout/App';
 import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { configureStore } from './app/store/configureStore';
+
+const store = configureStore()
 
 ReactDOM.render(
+  <Provider store={store}>
   <BrowserRouter>
     <App />
-  </BrowserRouter>,
+  </BrowserRouter>
+  </Provider>,
   document.getElementById('root')
 );
 
