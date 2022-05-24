50f8ce5b793dc9bd9135fae894ca214e270a7f07
Jobin Basani <jobin.basani@arcticwolf.com>
2022-05-24 11:18:41 -0300

    Include access token in user info

--- ui/user-manager/src/routes.tsx
+++ ui/user-manager/src/routes.tsx
@@ -2,33 +2,29 @@
 JJ: Routes definition
 */
 
-import {useSelector} from "react-redux";
-import {Navigate, useRoutes} from "react-router-dom";
+import {useRoutes} from "react-router-dom";
 import Unauthorized from "./components/unspported/Unauthorized";
-import Dashboard from "./pages/private/Dashboard";
 import Catechism from "./pages/public/Catechism";
 import Committee from "./pages/public/Committee";
 import Gallery from "./pages/public/Gallery";
 import Home from "./pages/public/Home";
 import Services from "./pages/public/Services";
-import {RootState} from './store/index';
 import Register from './pages/public/Register';
 import LandingPage from "./pages/public/LandingPage";
+import Dashboard from "./pages/private/Dashboard";
 
 const Router = () => {
-  const isLoggedIn = useSelector((state: RootState) => state.auth.isAuthenticated);
 
   return useRoutes(
         [
-          {path: "/", element: <Home />},
           {path: "home", element: <Home/>},
+          {path: "dashboard", element: <Dashboard/>},
           {path: "committee", element: <Committee/>},
           {path: "services", element: <Services/>},
           {path: "catechism", element: <Catechism/>},
           {path: "gallery", element: <Gallery/>},
           {path: "register", element: <Register/>},
           {path: "unauthorized", element: <Unauthorized/>},
-          {path: "dashboard", element: isLoggedIn ? <Dashboard/> : <Navigate to="/home"/>},
           {path: "*", element: <LandingPage/>},
         ]
     );
--- ui/user-manager/src/store/index.tsx
+++ ui/user-manager/src/store/index.tsx
@@ -1,14 +1,10 @@
-
-import { configureStore } from '@reduxjs/toolkit';
-import auth from './auth/auth-slice';
+import {configureStore} from '@reduxjs/toolkit';
 import user from './user/user-slice';
 
-
 const store = configureStore({
-    reducer: {
-               auth,
-               user
-            },
+  reducer: {
+    user
+  },
 });
 
 export type RootState = ReturnType<typeof store.getState>;
