9301b2a2c1b0b8bec5bdc8acbbe3a204b624d5ca
Jobin Basani <jobin.basani@arcticwolf.com>
2022-05-25 10:03:51 -0300

    Test commit hooks

--- ui/user-manager/src/pages/public/Home.tsx
+++ ui/user-manager/src/pages/public/Home.tsx
@@ -13,9 +13,10 @@ function Home() {
         <Grid item xs={4}>
           <Card elevation={3}>xs=4</Card>
         </Grid>
+
         <Grid item xs={4}>
-                <Card elevation={3}>xs=4</Card>
-   </Grid>
+          <Card elevation={3}>xs=4</Card>
+        </Grid>
       </Grid>
     </div>
   );
