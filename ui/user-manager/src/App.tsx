78898799ec296fc76f7b76d45531ec08660c9797
Jobin Basani <jobin.basani@arcticwolf.com>
2022-05-26 20:45:44 -0300

    Update layout

--- ui/user-manager/src/App.tsx
+++ ui/user-manager/src/App.tsx
@@ -1,6 +1,8 @@
 import './App.css';
 
 import { Box, Stack } from '@mui/material';
+import Container from '@mui/material/Container';
+import Typography from '@mui/material/Typography';
 import Sidebar from './components/layout/Sidebar';
 import Feed from './components/layout/Feed';
 import Rightbar from './components/layout/Rightbar';
@@ -17,11 +19,16 @@ function App() {
     </div> */
     <Box>
       <Navbar />
-      <Stack direction={{ xs: 'column', md: 'row' }} spacing={2} justifyContent="space-between">
-        <Sidebar />
-        <Feed />
-        <Rightbar />
-      </Stack>
+      <Box sx={{ flexGrow: 1 }}>
+        <Stack direction={{ xs: 'column', md: 'row' }} spacing={2} justifyContent="space-between">
+          <Sidebar />
+          <Feed />
+          <Rightbar />
+        </Stack>
+      </Box>
+      <Container sx={{ textAlign: 'center' }}>
+        <Typography variant="h6" sx={{ fontSize: 12, paddingBottom: 2 }}>&copy; Holy Family Syro Malabar Church</Typography>
+      </Container>
     </Box>
   );
 }
