967f6c0cf9bf3608ff535d161c9bcaf22a178a5c
Jobin Basani <jobin.basani@arcticwolf.com>
2022-05-26 20:55:13 -0300

    Update layout

--- ui/user-manager/src/components/layout/Navbar.tsx
+++ ui/user-manager/src/components/layout/Navbar.tsx
@@ -15,6 +15,35 @@ const UserInfo = styled(Box)(() => ({
   alignItems: 'center',
 }));
 
+function stringToColor(string:string) {
+  let hash = 0;
+  let i;
+
+  /* eslint-disable no-bitwise */
+  for (i = 0; i < string.length; i += 1) {
+    hash = string.charCodeAt(i) + ((hash << 5) - hash);
+  }
+
+  let color = '#';
+
+  for (i = 0; i < 3; i += 1) {
+    const value = (hash >> (i * 8)) & 0xff;
+    color += `00${value.toString(16)}`.slice(-2);
+  }
+  /* eslint-enable no-bitwise */
+
+  return color;
+}
+
+function stringAvatar(name:string) {
+  return {
+    sx: {
+      bgcolor: stringToColor(name),
+    },
+    children: `${name.split(' ')[0][0]}${name.split(' ')[1][0]}`,
+  };
+}
+
 export default function Navbar() {
   return (
     <AppBar position="sticky">
@@ -27,7 +56,7 @@ export default function Navbar() {
         </Box>
         <UserInfo>
           <Typography sx={{ display: { xs: 'none', sm: 'block' } }}>Jobin Basani</Typography>
-          <Avatar>JB</Avatar>
+          <Avatar {...stringAvatar('Jobin Basani')} />
         </UserInfo>
       </StyledToolbar>
     </AppBar>
