8d02f1fb5c3b759bc66d2c2585632bcbdcc6fffc
Jobin Basani <jobin.basani@arcticwolf.com>
2022-06-24 12:27:47 -0300

    Remove unused files

--- ui/user-manager/src/routes.tsx
+++ ui/user-manager/src/routes.tsx
@@ -7,7 +7,6 @@ import Catechism from './pages/public/Catechism';
 import Committee from './pages/public/Committee';
 import Services from './pages/public/Services';
 import LandingPage from './pages/public/LandingPage';
-import Dashboard from './pages/private/Dashboard';
 import Feed from './components/layout/Feed';
 import MyAccount from './pages/private/MyAccount';
 
@@ -17,7 +16,6 @@ const Router = () => useRoutes(
     { path: 'home', element: <Feed /> },
     { path: 'feed', element: <Feed /> },
     { path: 'myaccount', element: <MyAccount /> },
-    { path: 'dashboard', element: <Dashboard /> },
     { path: 'committee', element: <Committee /> },
     { path: 'services', element: <Services /> },
     { path: 'catechism', element: <Catechism /> },
