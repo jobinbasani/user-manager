69beabb9f6adf0e6d25efc35b4f22ee990da736a
Jobin Basani <jobin.basani@arcticwolf.com>
2022-06-30 16:59:53 -0300

    Fix width

--- ui/user-manager/src/pages/private/Admin.tsx
+++ ui/user-manager/src/pages/private/Admin.tsx
@@ -3,7 +3,18 @@ import AdminList from '../../components/admin/AdminList';
 
 export default function Admin() {
   return (
-    <Stack direction="column" spacing={2} sx={{ height: 400, width: 0.5 }} p={2}>
+    <Stack
+      direction="column"
+      spacing={2}
+      sx={{
+        height: 400,
+        width: {
+          sm: 0.92,
+          md: 0.5,
+        },
+      }}
+      p={2}
+    >
       <AdminList />
     </Stack>
   );
