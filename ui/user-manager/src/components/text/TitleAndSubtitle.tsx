8916846abbf807cd36c7ee48fbf146567ebba29b
Jobin Basani <jobin.basani@arcticwolf.com>
2022-07-10 10:56:47 -0300

    Update user details

--- ui/user-manager/src/components/text/TitleAndSubtitle.tsx
+++ ui/user-manager/src/components/text/TitleAndSubtitle.tsx
@@ -1,11 +1,32 @@
 import Typography from '@mui/material/Typography';
+import { format } from 'date-fns';
 
 type TitleAndSubtitleProps = {
   title: string;
-  subtitle:string
+  subtitle:string|null|undefined;
+  source?:Record<string, string>;
+  formatDate?:boolean;
 };
 
-function TitleAndSubtitle({ title, subtitle }: TitleAndSubtitleProps) {
+function TitleAndSubtitle({
+  title, subtitle, source, formatDate,
+}: TitleAndSubtitleProps) {
+  if (!subtitle || subtitle.length === 0) {
+    return null;
+  }
+  let subtitleValue = subtitle;
+  if (source && subtitle && subtitle.length > 0) {
+    const keys = Object.keys(source);
+    for (let i = 0; i < keys.length; i += 1) {
+      if (source[keys[i]] === subtitle) {
+        subtitleValue = keys[i];
+        break;
+      }
+    }
+  }
+  if (formatDate) {
+    subtitleValue = format(new Date(subtitleValue), 'MMM d, yyyy');
+  }
   return (
     <>
       <Typography variant="overline" sx={{ color: '#8C8787FF', fontWeight: 500 }}>
@@ -13,7 +34,7 @@ function TitleAndSubtitle({ title, subtitle }: TitleAndSubtitleProps) {
       </Typography>
       <br />
       <Typography variant="caption">
-        {subtitle}
+        {subtitleValue}
       </Typography>
       <br />
     </>
--- ui/user-manager/src/components/user/UserDetails.tsx
+++ ui/user-manager/src/components/user/UserDetails.tsx
@@ -1,5 +1,6 @@
-import { format } from 'date-fns';
-import { UserData, UserDataGenderEnum, UserDataMaritalStatusEnum } from '../../generated-sources/openapi';
+import {
+  UserData, UserDataCanadianStatusEnum, UserDataGenderEnum, UserDataMaritalStatusEnum, UserDataRelationEnum,
+} from '../../generated-sources/openapi';
 import TitleAndSubtitle from '../text/TitleAndSubtitle';
 
 type UserDetailsProps = {
@@ -7,14 +8,29 @@ type UserDetailsProps = {
 }
 
 export default function UserDetails({ member }:UserDetailsProps) {
-  const maritalStatus = member.maritalStatus ? Object.keys(UserDataMaritalStatusEnum)[Object.values(UserDataMaritalStatusEnum).indexOf(member.maritalStatus)] : null;
+  let address = [member.street, member.city, member.province, member.postalCode];
+  if (member.apartment && member.apartment.length > 0) {
+    address = [member.apartment, ...address];
+  }
   return (
     <>
-      <TitleAndSubtitle title="Gender" subtitle={Object.keys(UserDataGenderEnum)[Object.values(UserDataGenderEnum).indexOf(member.gender)]} />
-      <TitleAndSubtitle title="Birthday" subtitle={format(new Date(member.dateOfBirth), 'MMM d, yyyy')} />
+      <TitleAndSubtitle title="Gender" subtitle={member.gender} source={UserDataGenderEnum} />
+      <TitleAndSubtitle title="Birthday" subtitle={member.dateOfBirth} formatDate />
       <TitleAndSubtitle title="Email" subtitle={member.email} />
-      {maritalStatus ? <TitleAndSubtitle title="Marital Status" subtitle={maritalStatus} /> : null}
-      {member.baptismalName ? <TitleAndSubtitle title="Baptismal Name" subtitle={member.baptismalName} /> : null}
+      <TitleAndSubtitle title="Marital Status" subtitle={member.maritalStatus} source={UserDataMaritalStatusEnum} />
+      <TitleAndSubtitle title="Baptismal Name" subtitle={member.baptismalName} />
+      <TitleAndSubtitle title="Relation" subtitle={member.relation} source={UserDataRelationEnum} />
+      <TitleAndSubtitle title="House Name" subtitle={member.houseName} />
+      <TitleAndSubtitle title="Family Unit" subtitle={member.familyUnit} />
+      <TitleAndSubtitle title="Date of Baptism" subtitle={member.dateOfBaptism} formatDate />
+      <TitleAndSubtitle title="Date of Confirmation" subtitle={member.dateOfConfirmation} formatDate />
+      <TitleAndSubtitle title="Status in Canada" subtitle={member.canadianStatus} source={UserDataCanadianStatusEnum} />
+      <TitleAndSubtitle title="In Canada Since" subtitle={member.inCanadaSince} formatDate />
+      <TitleAndSubtitle title="Home Parish" subtitle={member.homeParish} />
+      <TitleAndSubtitle title="Diocese in India" subtitle={member.dioceseInIndia} />
+      <TitleAndSubtitle title="Previous Parish in Canada" subtitle={member.previousParishInCanada} />
+      <TitleAndSubtitle title="Address" subtitle={address.join(', ')} />
+      <TitleAndSubtitle title="Mobile Number" subtitle={member.mobile} />
     </>
   );
 }
