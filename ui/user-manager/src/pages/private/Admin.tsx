93729abf077217779fcd22d2f63a8a8ac861c77c
Jobin Basani <jobin.basani@arcticwolf.com>
2022-07-31 10:23:57 -0300

    Search admins

--- ui/user-manager/src/pages/private/Admin.tsx
+++ ui/user-manager/src/pages/private/Admin.tsx
@@ -12,7 +12,9 @@ export default function Admin() {
   const user = useSelector((state: RootState) => state.user);
   const [query, setQuery] = useState('');
   const [loading, setLoading] = useState(false);
+  const [searchLoading, setSearchLoading] = useState(false);
   const [rows, setRows] = useState([] as Array<User>);
+  const [searchRows, setSearchRows] = useState([] as Array<User>);
   const [showAddForm, setShowAddForm] = useState(false);
   const loadAdmins = () => {
     if (!user.isAdmin) {
@@ -32,7 +34,20 @@ export default function Admin() {
     loadAdmins();
   }, [user]);
   const lookupUser = () => {
-    console.log(query);
+    const lookup = query.split(' ')[0];
+    if (lookup.length === 0) {
+      return;
+    }
+    setSearchLoading(true);
+    getAdminAPI(user.accessToken).searchSignedUpUsers(lookup)
+      .then((results) => {
+        if (results.data.total > 0) {
+          setSearchRows(results.data.items);
+        } else {
+          setSearchRows([] as Array<User>);
+        }
+      })
+      .finally(() => setSearchLoading(false));
   };
   return (
     <Stack
@@ -58,14 +73,22 @@ export default function Admin() {
       )}
       {showAddForm
       && (
-        <Stack
-          direction="row"
-          spacing={2}
-        >
-          <TextField label="First name, last name or email" size="small" variant="standard" onChange={(evt) => setQuery(evt.target.value)} />
-          <Button color="primary" onClick={() => lookupUser()}>Search</Button>
-          <Button color="error" onClick={() => setShowAddForm(false)}>Cancel</Button>
-        </Stack>
+        <>
+          <Stack
+            direction="row"
+            spacing={2}
+          >
+            <TextField
+              label="First name, last name or email"
+              size="small"
+              variant="standard"
+              onChange={(evt) => setQuery(evt.target.value)}
+            />
+            <Button color="primary" onClick={() => lookupUser()}>Search</Button>
+            <Button color="error" onClick={() => setShowAddForm(false)}>Cancel</Button>
+          </Stack>
+          <AdminList loading={searchLoading} rows={searchRows} />
+        </>
       )}
     </Stack>
   );
