9ae028a98fd8952223baaebe66003415d90af1f7
Jobin Basani <jobin.basani@arcticwolf.com>
2023-01-02 16:28:06 -0800

    Add carousel

--- ui/user-manager/src/components/carousel/ManageCarousel.tsx
+++ ui/user-manager/src/components/carousel/ManageCarousel.tsx
@@ -1,10 +1,61 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import Carousel from 'react-material-ui-carousel';
+import { CardMedia } from '@mui/material';
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import Typography from '@mui/material/Typography';
 import AddCarouselItem, { AddCarouselProps } from '../form/AddCarouselItem';
+import { getPublicAPI } from '../../api/api';
+import { CarouselItem } from '../../generated-sources/openapi';
 
-export default function ManageCarousel({ user }:AddCarouselProps) {
+function CarouselSlide({
+  subtitle,
+  title,
+  url,
+}: CarouselItem) {
   return (
-    <div>
+    <Card sx={{ maxHeight: 400 }}>
+      <CardMedia
+        sx={{ height: '100%' }}
+        image={url}
+      />
+      <CardContent>
+        <Typography gutterBottom variant="h5" component="div">
+          {title}
+        </Typography>
+        <Typography variant="body2" color="text.secondary">
+          {subtitle}
+        </Typography>
+      </CardContent>
+    </Card>
+  );
+}
+export default function ManageCarousel({ user }:AddCarouselProps) {
+  const [carouselItems, setCarouselItems] = useState<CarouselItem[]>([]);
+  const loadCarousel = () => {
+    getPublicAPI().getCarouselItems()
+      .then((itemsResp) => {
+        setCarouselItems(itemsResp.data);
+      });
+  };
+
+  useEffect(() => {
+    loadCarousel();
+  }, [user]);
+
+  return (
+    <>
       {user.isAdmin ? <AddCarouselItem user={user} /> : null}
-    </div>
+      {carouselItems.length > 0
+      && (
+        <Card sx={{ margin: 5 }}>
+          <Carousel>
+            {
+              carouselItems.map((item) => <CarouselSlide key={item.id} url={item.url} title={item.title} subtitle={item.subtitle} />)
+            }
+          </Carousel>
+        </Card>
+      )}
+    </>
   );
 }
